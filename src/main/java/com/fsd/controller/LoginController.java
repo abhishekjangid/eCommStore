a2a03c9429a75278ee914e73a75eb7d0ab9a1666
abhishekjangid <abhishekp.jangid@gmail.com>
2021-08-06 14:50:09 +0530

    Added Validations

--- src/main/java/com/fsd/controller/LoginController.java
+++ src/main/java/com/fsd/controller/LoginController.java
@@ -15,14 +15,32 @@ public class LoginController {
         System.out.println("+================================+");
 
         System.out.println("Enter user id and Password:");
-        System.out.println("User Id : ");
-        String userId = consoleScan.nextLine();
-        System.out.println("Password : ");
-        String password = consoleScan.nextLine();
-        System.out.println();
+        String userId = "";
+        String password = "";
+        boolean flag = true;
+        boolean isValidCredentails = true;
+        while(flag) {
+            System.out.println("User Id : ");
+            userId = consoleScan.nextLine();
+            System.out.println("Password : ");
+            password = consoleScan.nextLine();
+            System.out.println();
+            isValidCredentails = !validate(Patterns.USERNAME.matcher(userId)::matches)
+                    || !validate(Patterns.NAME.matcher(userId)::matches);
+            if(isValidCredentails) {
+                System.out.println("Bad Input, Do you want to re-enter credentials Y/N");
+                String retry = consoleScan.nextLine();
+                if(retry != null && retry.equalsIgnoreCase("y")){
+                    continue;
+                }
+            }
+            flag = false;
+        }
 
-        SecurityContext context = new LoginServiceImpl().login(userId, password);
-
+        SecurityContext context;
+        if(isValidCredentails) {
+            context = new LoginServiceImpl().login(userId, password);
+        }
         System.out.println("+================================+");
         if(context != null) {
             System.out.println("| Welcome " + context.getUserName());
